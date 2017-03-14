5e25dae84de58b5b6466c8c520f542b27ae93794
necisam <king4sam990816@gmail.com>
2017-03-14 23:27:01 +0800

    implements encry

--- double_trans_cipher.rb
+++ double_trans_cipher.rb
@@ -7,9 +7,34 @@ module DoubleTranspositionCipher
     # 3. sort rows in predictibly random way using key as seed
     # 4. sort columns of each row in predictibly random way
     # 5. return joined cyphertext
+
+    rows = Math.sqrt(document.length).ceil
+    cols = (document.length.to_f/row).ceil
+
+    ex_document = document.length < rows * cols ?  document.ljust(rows * cols, ' ') : document
+
+    matrix = ex_document.chars.each_slice(cols).to_a
+    row_shuffle_index = (0...rows).to_a.shuffle(random: Random.new(key))
+    cols_shuffle_index = (0...cols).to_a.shuffle(random: Random.new(key))
+
+    matrix.sort_by!.with_index do |_,index|
+      row_shuffle_index[index]
+    end
+
+    matrix.each do |row|
+      row.sort_by!.with_index do |_,index|
+        cols_shuffle_index[index]
+      end
+    end
+
+    matrix.inject('') do |encrypt_doc, row|
+      encrypt_doc << row.join
+    end
   end
 
   def self.decrypt(ciphertext, key)
     # TODO: FILL THIS IN!
+    ciphertext
   end
+
 end
