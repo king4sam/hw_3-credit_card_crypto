e06cc07da1de70f12019145b84d36e88d608e187
necisam <king4sam990816@gmail.com>
2017-03-19 14:45:50 +0800

    spec for ModernSymmetricCipher

--- spec/crypto_spec.rb
+++ spec/crypto_spec.rb
@@ -1,6 +1,7 @@
 require_relative '../credit_card'
 require_relative '../substitution_cipher'
 require_relative '../double_trans_cipher.rb'
+require_relative '../sk_cipher.rb'
 require 'minitest/autorun'
 
 describe 'Test card info encryption' do
@@ -49,6 +50,29 @@ describe 'Test card info encryption' do
     it 'should decrypt text' do
       encrypt_context = DoubleTranspositionCipher.encrypt(@cc.to_s, @key)
       decrypt_context = DoubleTranspositionCipher.decrypt(encrypt_context, @key)
+    end
+  end
+
+  describe 'Using ModernSymmetricCipher' do
+    it 'should generate a new Base64 string key' do
+      new_key = ModernSymmetricCipher.generate_new_key
+      new_key.must_be_kind_of String
+      new_key.wont_be_nil
+      new_key.wont_be_empty
+    end
+
+    it 'should encrypt card information' do
+      new_key = ModernSymmetricCipher.generate_new_key
+      enc = ModernSymmetricCipher.encrypt(@cc.to_s, new_key)
+      enc.wont_equal @cc.to_s
+      enc.wont_be_nil
+    end
+
+    it 'should decrypt text' do
+      new_key = ModernSymmetricCipher.generate_new_key
+      enc = ModernSymmetricCipher.encrypt(@cc.to_s, new_key)
+      dec = ModernSymmetricCipher.decrypt(enc, new_key)
+      dec.must_equal @cc.to_s
     end
   end
 end
