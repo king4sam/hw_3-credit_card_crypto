4e83d32d39e2d97310a9415f3dde67f63ac78c7d
necisam <king4sam990816@gmail.com>
2017-03-19 16:18:23 +0800

    test for creditcard.hash/hash_secure

--- spec/hash_spec.rb
+++ spec/hash_spec.rb
@@ -39,14 +39,34 @@ describe 'Test hashing requirements' do
 
   describe 'Test cryptographic hashing' do
     describe 'Check hashes are consistently produced' do
+      it 'should be the same and not nil' do
+        db = cards.map {|c| [c,c.clone] }
+        db.each do |cp|
+          cp.first.hash_secure.wont_be_nil
+          cp.last.hash_secure.wont_be_nil
+          cp.first.hash_secure.must_equal cp.last.hash_secure
+        end
+      end
       # TODO: Check that each card produces the same hash if hashed repeatedly
     end
 
     describe 'Check for unique hashes' do
+      it 'should be different for different cards' do
+        cards.combination(2).to_a.each do |cmp|
+          cmp.first.hash_secure.wont_be_nil
+          cmp.last.hash_secure.wont_be_nil
+          cmp.first.hash_secure.wont_equal cmp.last.hash_secure
+        end
+      end
       # TODO: Check that each card produces a different hash than other cards
     end
 
     describe 'Check regular hash not same as cryptographic hash' do
+      it 'hash should not be the same as hash_secure' do
+        cards.each do |card|
+          card.hash.wont_equal card.hash_secure
+        end
+      end
       # TODO: Check that each card's hash is different from its hash_secure
     end
   end
